d81d9e30b5ad0c23ead6bcd5747ef6792f1df4fb
SNEHABHIxd <93216312+SNEHABHIxd@users.noreply.github.com>
2021-12-05 20:13:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,4 +4,4 @@ from os import getenv
 class Config(object):
       BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
       BOT_ID = os.environ.get("BOT_TOKEN", "")
-      COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ ! .")
+      COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ ! .").split())
